8322725cfc40646a74b5bd56e8a01b184d19eda8
Patrick Dawkins <pjcdawkins@gmail.com>
2015-04-03 11:02:48 +0100

    Add source() method

--- includes/PlatformshApiResource.php
+++ includes/PlatformshApiResource.php
@@ -8,4 +8,29 @@ class PlatformshApiResource extends Entity {
   public $url;
   public $refreshed;
   public $data = array();
+
+  protected $source;
+
+  /**
+   * Get the source object for the resource.
+   *
+   * @throws \Exception
+   *   If an appropriate model class cannot be found.
+   *
+   * @return \Platformsh\Client\Model\Resource
+   *   An instance of one of the model classes extending the Resource class, for
+   *   example \Platformsh\Client\Model\Subscription.
+   */
+  public function source() {
+    if (!isset($this->source)) {
+      $client = platformsh_api_client();
+      $className = '\\Platformsh\\Client\\Model\\' . $this->type;
+      if (!class_exists($className)) {
+        throw new \Exception("Model class not found: $className");
+      }
+      $this->source = new $className($this->data, $this->url, $client->getConnector()->getClient());
+    }
+
+    return $this->source;
+  }
 }
